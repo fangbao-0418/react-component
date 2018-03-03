5cec34c9e055401701b0b1e94312404e018d3f71
fangbao <fangbao@i-counting.cn>
2018-03-03 09:56:20 +0800

    modal

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -1,20 +1,28 @@
 import $ from 'jquery'
+import React from 'react'
+import { findDOMNode, render } from 'react-dom'
+
+console.log(React)
 export interface MyOptions {
-  title?: string
+  title?: string,
+  content?: any
 }
 class Modal {
   public title: string = 'Modal'
+  public content: any = <div>no message</div>
   public $el = $(document.createElement('div'))
   public defaultCls = 'pilipa-modal'
   public pageX = 0
   public pageY = 0
   public constructor (options: MyOptions = {}) {
     this.title = options.title || this.title
+    this.content = options.content || this.content
     this.$el.addClass(this.defaultCls)
     $('body').on('click', (event) => {
       this.pageX = event.pageX
-      console.log(event)
+      this.pageY = event.pageY
     })
+    console.log(options.content)
   }
   public initEvent () {
     this.$el.find('.pilipa-modal-close').unbind('click').click(() => {
@@ -28,11 +36,36 @@ class Modal {
 
       }
     })
+    this.$el.find('.pilipa-modal-wrap').off('click').on('click', (event) => {
+      if (this.$el.children('.pilipa-modal-wrap').find(event.target).length === 0) {
+        this.hide()
+      }
+    })
+  }
+  public setTransformOrigin () {
+    const offset = this.$el.find('.pilipa-modal-content').offset()
+    const w = this.$el.find('.pilipa-modal-content').width()
+    const h = this.$el.find('.pilipa-modal-content').height()
+    let x = 0
+    if (offset.left + w/2 > this.pageX) {
+      x = offset.left - this.pageX
+    } else {
+      x = -(this.pageX - offset.left)
+    }
+    const y = offset.top - this.pageY
+    this.$el.find('.pilipa-modal-content').css({
+      transformOrigin: `${-x}px ${-y}px 0px`
+    })
   }
   public show () {
-    console.log(this.pageX)
-    this.$el.html(this.template())
     $('body').append(this.$el)
+    this.$el.html(this.template())
+    render(
+      this.content,
+      this.$el.find('.pilipa-modal-body')[0]
+    )
+
+    this.setTransformOrigin()
     this.$el.find('.pilipa-modal-mask').addClass('pilipa-fade-enter pilipa-fade-active')
     this.$el.find('.pilipa-modal-content').addClass('pilipa-zoom-enter pilipa-zoom-active')
     setTimeout(() => {
@@ -56,7 +89,7 @@ class Modal {
     return `
       <div class="${this.defaultCls}-mask"></div>
       <div class="${this.defaultCls}-wrap">
-        <div class="${this.defaultCls}-content" style="transform-origin: 250px -50% 0px;">
+        <div class="${this.defaultCls}-content" style="">
           <div class="${this.defaultCls}-header">
             <span class="${this.defaultCls}-title">${this.title}</span>
             <span class="${this.defaultCls}-close">×</span>
--- dev/modal.js
+++ dev/modal.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { findDOMNode } from 'react-dom'
 import modal from '../components/modal'
+class Content extends React.Component {
+  toClick () {
+    console.log(this)
+    alert('xxx')
+  }
+  render () {
+    return (
+      <div>
+        Content Component
+        <button onClick={this.toClick.bind(this)}>点我</button>
+      </div>
+    )
+  }
+}
 export default class extends React.Component {
   constructor () {
     super()
     this.modal = new modal({
-      title: '修改文字'
+      title: '修改文字',
+      content: '<div>xxxx33</div>'
     })
     this.modal2 = new modal()
     // this.modal.show({
@@ -24,6 +40,35 @@ export default class extends React.Component {
     return (
       <div>
         <button onClick={this.toClick.bind(this)}>打开</button>
+        <button
+          onClick={this.toClick.bind(this)}
+          style={{
+            position: 'absolute',
+            right: 0
+          }}
+        >
+          打开
+        </button>
+        <button
+          onClick={this.toClick.bind(this)}
+          style={{
+            position: 'absolute',
+            right: '50%',
+            top: '0'
+          }}
+        >
+          打开
+        </button>
+        <button
+          onClick={this.toClick.bind(this)}
+          style={{
+            position: 'absolute',
+            right: '50%',
+            top: '500px'
+          }}
+        >
+          打开
+        </button>
         <button onClick={this.toClose.bind(this)}>关闭</button>
       </div>
     )
